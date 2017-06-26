fc37b0c19fcdddf7cbe759b0dccf7a1d092738f8
Pablo Sergio Garcia <pablosergiogarcia@gmail.com>
2017-06-26 11:11:24 -0300

    Creacion ubicacion geografica

--- src/factura/Cliente.java
+++ src/factura/Cliente.java
@@ -11,7 +11,52 @@ package factura;
  */
 public class Cliente {
     private String nombre;
+    private String apellido;
+    private Integer dni;
+    private String calle;
+    private Integer altura;
+    private Localidad nombre_localidad;
 
+    public Localidad getNombre_localidad() {
+        return nombre_localidad;
+    }
+
+    public void setNombre_localidad(Localidad nombre_localidad) {
+        this.nombre_localidad = nombre_localidad;
+    }
+            
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Integer altura) {
+        this.altura = altura;
+    }
+    
     public String getNombre() {
         return nombre;
     }
