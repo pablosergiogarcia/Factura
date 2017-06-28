9cdc133b1db123edcc5c3b9b7b15b4d04603f7d4
Pablo Sergio Garcia <pablosergiogarcia@gmail.com>
2017-06-28 10:13:52 -0300

    Forma de pago agregado

--- src/factura/Factura.java
+++ src/factura/Factura.java
@@ -19,7 +19,7 @@ import java.util.Set;
 public class Factura {
 
     private ArrayList<ItemFactura> itemFactura = new ArrayList();    
-    private FormaCobro formaCobro;
+    private ArrayList<FormaCobro> formaCobro = new ArrayList();
     private Cliente cliente;
     private Date fecha;
     private Integer numero;
@@ -31,15 +31,12 @@ public class Factura {
        itemFactura.add(pItemFactura);
     }
     
-    
-
-    public FormaCobro getFormaCobro() {
+    public ArrayList<FormaCobro> getItemFormaCobro() {
         return formaCobro;
     }
-
-    public void setFormaCobro(FormaCobro formaCobro) {
-        this.formaCobro = formaCobro;
-    }
+    public void formaCobro(FormaCobro pFormaCobro){
+       formaCobro.add(pFormaCobro);
+    }    
 
     public Cliente getCliente() {
         return cliente;
@@ -69,45 +66,6 @@ public class Factura {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
-        Cliente unCliente = new Cliente();
-        unCliente.setNombre("Manuel");
-        
-        FormaCobro unFormaCobro = new FormaCobro();
-        unFormaCobro.setNombre("Visa");
-        
-        Item cocaCola = new Item();
-        cocaCola.setNombre("Coca cola");
-        cocaCola.setPrecio(12.43);
 
-        Item pepsi = new Item();
-        pepsi.setNombre("Pepsi");
-        pepsi.setPrecio(15.43);
-
-        
-        ItemFactura unItemFacturaPepsi = new ItemFactura();
-        unItemFacturaPepsi.setCantidad(2.0);
-        unItemFacturaPepsi.setItem_factura(pepsi);
-        unItemFacturaPepsi.setPrecio(pepsi.getPrecio());
-        
-        ItemFactura unItemFacturaCoca = new ItemFactura();
-        unItemFacturaCoca.setCantidad(10.0);
-        unItemFacturaCoca.setItem_factura(cocaCola);
-        unItemFacturaCoca.setPrecio(cocaCola.getPrecio());
-        
-        
-        
-        Factura unaFactura = new Factura();
-        unaFactura.setCliente(unCliente);
-        unaFactura.setNumero(2354);
-        unaFactura.setFormaCobro(unFormaCobro);
-        unaFactura.agregarItem(unItemFacturaPepsi);
-        unaFactura.agregarItem(unItemFacturaCoca);
-    
-        System.out.println(unaFactura.getItemFactura().get(0).getItem_factura().getPrecio());
-        
-        
-    }
     
 }
